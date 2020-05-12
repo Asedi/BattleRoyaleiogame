c0bf090da16ac7c47125017707b70df70e6fb01c
carpetkagtasa@gmail.com <carpetkagtasa@gmail.com>
2020-05-12 07:45:19 +0200

    Add player attack animation

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,37 +3,41 @@ extends KinematicBody2D
 const GRAVITY = 20.0
 const WALK_SPEED = 10
 const MAXIMUM_SPEED = 120
-const JUMP_STRENGTH=300
-onready var animplayer=get_node("Playerdir/Player/AnimationPlayer") 
-onready var playerdir=get_node("Playerdir")
+const JUMP_STRENGTH= 300
+onready var playerdir= get_node("Playerdir")
+onready var animationTree= $AnimationTree
+onready var katana= get_node("Playerdir/Player/leftArm/katana")
+var weaponEquipped=false
 var velocity = Vector2()
 
 func _physics_process(delta):
 	velocity.y += GRAVITY
-
+	if Input.is_action_just_released("equip")&&weaponEquipped:
+		weaponEquipped=false
+		animationTree.set("parameters/runWithWeapon/blend_amount",0)
+		katana.hide()
+	elif Input.is_action_just_released("equip")&&!weaponEquipped:
+		weaponEquipped=true
+		animationTree.set("parameters/runWithWeapon/blend_amount",1)
+		katana.show()
 	if Input.is_action_pressed("ui_left"):
+		animationTree.set("parameters/runIdleJump/blend_amount",-1)
 		velocity.x = max(velocity.x-WALK_SPEED, -MAXIMUM_SPEED)
 		playerdir.set_scale(Vector2(1,1))
-		animplayer.play("run")
-		if is_on_floor():
-			animplayer.play("run")
-		else:
-			animplayer.play("jump")
 	elif Input.is_action_pressed("ui_right"):
+		animationTree.set("parameters/runIdleJump/blend_amount",-1)
 		velocity.x = min(velocity.x+WALK_SPEED, +MAXIMUM_SPEED)
 		playerdir.set_scale(Vector2(-1,1))
-		if is_on_floor():
-			animplayer.play("run")
-		else:
-			animplayer.play("jump")
 	else:
-		animplayer.play("idle")
+		animationTree.set("parameters/runIdleJump/blend_amount",0)
 		velocity.x = lerp(velocity.x, 0, 0.2)
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			animplayer.play("idle")
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y -= JUMP_STRENGTH
-
+	else:
+		animationTree.set("parameters/runIdleJump/blend_amount",1)
+	if Input.is_action_just_pressed("attack"):
+		animationTree.set("parameters/attack/active",true)
 	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
 
 	# The second parameter of "move_and_slide" is the normal pointing up.
